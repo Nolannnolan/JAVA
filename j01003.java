b98a2a89c5d8afbd54bfc9b0d400cc2e2812a7f3
Nolannnolan <thngoc16102003@gmail.com>
2024-02-17 16:51:32 +0700

    JAVA

---
+++ j01003.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+public class j01003 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        if (a == 0){
+            if(b == 0) System.out.println("VSN");
+            else System.out.println("VN");
+        }
+        else{
+            double x = -b / a;
+            System.out.format("%.2f", x);
+        }
+    }
+}
